ceed3425d0488136575e5f6beb29809cd09abde2
shadichy <shadichy@github.com>
2021-08-10 17:54:08 +0700

    shell

--- ins.sh
+++ ins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 net=0
 nct=0
+workdir=$(pwd)
+bt="ExtOS-respin Installer"
+tt="Installing progres"
 netcheck () {
     if [ $net -eq 0 ]; then
         clear
@@ -27,7 +30,7 @@ netcheck () {
     fi
 }
 diskchoose () {
-    disk=$( dialog --backtitle 'ExtOS-respin Installer' --title 'Installing progress' --stdout --menu "Disk/partition options" 10 40 10 1 "Open partition editor tool" 2 "Select disk and install" )
+    disk=$(dialog --backtitle "$bt" --title "$tt" --stdout --menu "Disk/partition options" 0 0 0 1 "Open partition editor tool" 2 "Select disk and install")
     if [ $disk -eq 1 ]; then
         clear
         printf "Use 'fdisk' or 'parted' commands to edit the disks/partitions\n"
@@ -35,12 +38,36 @@ diskchoose () {
         printf "Type 'exit' to continue\n"
         printf "\n"
         $SHELL
+        disksel
     elif [ $disk -eq 2 ]; then
-        dialog --backtitle 'ExtOS-respin Installer' --title 'Installing progress' --msgbox 'nah' 10 40
+        disksel
     else
         net=0
         netinit
     fi
+}
+disksel () {
+    clear
+    fdisk -l
+    printf "\nex: Type in '/dev/sda', '/dev/hdb2', 'xvda1', vdc3', 'nvme0n3p2',... ('full path and name' or 'name only', no brackets), or whatever nvme, disk, sdcard, virtual mem or specify any partions exist in the list above\n\n"
+    read -p "Choose disk/partition to install: " diskmnt
+    if [ -b "/dev/$diskmnt" ]; then
+        disktaget="/dev/$diskmnt"
+        ossel
+    elif [ -b $diskmnt ]; then
+        disktaget=$diskmnt
+        ossel
+    elif [ $diskmnt == "exit" ] || [ $diskmnt == "quit" ] || [ $diskmnt == "q" ]; then
+        diskchoose
+    elif [ -b "/dev/$diskmnt" ] || [ ! -b $diskmnt ]; then
+        printf "Invalid disk/partition path/name"
+        sleep 3
+        disksel
+    fi
+}
+ossel () {
+    dialog --backtitle "$bt" --title "$tt" --menu 0 0 0 1 "Arch-based" 2 "Ubuntu-based"
+    echo lol
 }
 netinit () {
     if wget -q --spider http://archlinux.org; then
@@ -52,7 +79,7 @@ netinit () {
         diskchoose
     else
         if [ $nct -eq 1 ]; then
-            if dialog --backtitle 'ExtOS-respin Installer' --title 'Installing progress' --yesno "Continue without network?" 10 40 ; then
+            if dialog --backtitle "$bt" --title "$tt" --yesno "Continue without network?" 0 0 ; then
                 net=1
                 diskchoose
             fi
@@ -60,13 +87,14 @@ netinit () {
         netcheck
     fi
 }
-if dialog --backtitle 'ExtOS-respin Installer' --title 'Installing progress' --yesno "This is ExtOS linux respin v0.1\nMade by Shadichy\n\nStart installation process?" 20 50
+if dialog --backtitle "$bt" --title "$tt" --yesno "This is ExtOS linux respin v0.1\nMade by Shadichy\n\nStart installation process?" 0 0
 then
     netinit
 else
-    echo "Run ./install.sh to restart the installer"
+    clear
+    printf "Run ./install.sh to restart the installer"
     exit
 fi
 echo "let's continue"
-#locale=$( dialog --backtitle "ExtOS-respin Installer" --title "Installing progress" --msgbox "check" 10 40 3>&1 1>&2 2>&3 3>&- )
+#locale=$( dialog --backtitle "$bt" --title "$tt" --msgbox "check" 10 40 3>&1 1>&2 2>&3 3>&- )
 #echo "$locale" >> ./out
